5f50462a68792a54b324350c13a3d6142e575e8e
Gabriel <onlinehassle1234@gmail.com>
2025-03-27 13:00:03 +0100

    main file commit

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR push to cars
+          Edit <code>src/App.tsx</code> and save to test HMR push to Main file
         </p>
       </div>
       <p className="read-the-docs">
